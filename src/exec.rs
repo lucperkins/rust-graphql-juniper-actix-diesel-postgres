779448eefc5590fbe57451e9a7f1b57adfcb4fbb
lucperkins <lucperkins@gmail.com>
2019-12-29 15:35:48 -0500

    Add new GraphQLExecutor actor

---
+++ src/exec.rs
@@ -0,0 +1,36 @@
+use actix::{Actor, Addr, Handler, Message, SyncContext};
+use actix_web::Error;
+use juniper::http::GraphQLRequest;
+use std::sync::Arc;
+use super::context::GraphQLContext;
+use super::graphql::Schema;
+
+#[derive(Deserialize, Serialize)]
+pub struct GraphQLData(GraphQLRequest);
+
+impl Message for GraphQLData {
+    type Result = Result<String, Error>;
+}
+
+pub struct GraphQLExecutor {
+    schema: Arc<Schema>,
+    context: GraphQLContext,
+}
+
+impl Actor for GraphQLExecutor {
+    type Context = SyncContext<Self>;
+}
+
+impl Handler<GraphQLData> for GraphQLExecutor {
+    type Result = Result<String, Error>;
+
+    fn handle(&mut self, msg: GraphQLData, _: &mut Self::Context) -> Self::Result {
+        let res = msg.0.execute(&self.schema, &self.context);
+        let res_str = serde_json::to_string(&res)?;
+        Ok(res_str)
+    }
+}
+
+pub struct State {
+    exec: Addr<GraphQLExecutor>,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
-#[macro_use]
-extern crate diesel;
+#[macro_use] extern crate diesel;
+#[macro_use] extern crate serde;
 
 pub mod context;
 pub mod data;
 pub mod db;
 pub mod endpoints;
+pub mod exec;
 pub mod graphql;
 pub mod models;
 pub mod schema;
